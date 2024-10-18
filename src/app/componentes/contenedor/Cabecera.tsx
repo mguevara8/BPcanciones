adc46e572593c2f50da6366a88abf805b0e62a44
mguevara8 <mguevara8@cuc.edu.co>
2024-10-17 21:01:44 -0500

    hasta cabecera

--- src/app/componentes/contenedor/Cabecera.tsx
+++ src/app/componentes/contenedor/Cabecera.tsx
@@ -1,11 +1,14 @@
+import { NavLink } from "react-router-dom";
+import milogo from "../../../assets/img/logoReact.png"
+
 export const Cabecera = () => {
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar
-          </a>
+          <NavLink className="navbar-brand" to="/">
+            <img src={milogo} alt="El logo" />
+          </NavLink>
           <button
             className="navbar-toggler"
             type="button"
@@ -20,15 +23,17 @@ export const Cabecera = () => {
           <div className="collapse navbar-collapse" id="navbarColor02">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
+                <NavLink className="nav-link active" aria-current="page" to="/">
                   Inicio
-                </a>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Autores
-                </a>
+                <NavLink className="nav-link" to="*">
+                  Compositores
+                </NavLink>
               </li>
+
+
               <li className="nav-item dropdown">
                 <a
                   className="nav-link dropdown-toggle"
@@ -41,29 +46,29 @@ export const Cabecera = () => {
                 </a>
                 <ul className="dropdown-menu">
                   <li>
-                    <a className="dropdown-item" href="#">
+                    <NavLink className="dropdown-item" to="/Cancre">
                      Regristar canciones
-                    </a>
+                    </NavLink>
                   </li>
                   <li>
-                    <a className="dropdown-item" href="#">
+                    <NavLink className="dropdown-item" to="/Canlis">
                      Listar canciones
-                    </a>
+                    </NavLink>
                   </li>
                   <li>
                     <hr className="dropdown-divider" />
                   </li>
                   <li>
-                    <a className="dropdown-item" href="#">
+                    <NavLink className="dropdown-item" to="/Canadmin">
                       Administrar canciones
-                    </a>
+                    </NavLink>
                   </li>
                 </ul>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <NavLink className="nav-link" to="/acer">
                   Acerca de
-                </a>
+                </NavLink>
               </li>
             </ul>
             <form className="d-flex" role="search">
--- src/app/utilidades/rutas/Ruteo.tsx
+++ src/app/utilidades/rutas/Ruteo.tsx
@@ -5,19 +5,22 @@ import { CanListar } from "../../componentes/canciones/CanListar";
 import { CanAdmin } from "../../componentes/canciones/CanAdmin";
 import { CanActualizar } from "../../componentes/canciones/CanActualizar";
 import { NoEncontrado } from "../../componentes/contenedor/NoEncontrado";
+import { Acerca } from "../../componentes/otros/Acerca";
 
 
 export const Ruteo = ()=>{
     return(
        <Routes>
-        <Route path="" element={<Inicio />}/>
+        <Route path="/" element={<Inicio />}/>
 
-        <Route path="Cancre" element={<CanCrear />}/>
-        <Route path="Canlis" element={<CanListar />}/>
-        <Route path="Canadmin" element={<CanAdmin />}/>
+        <Route path="/Cancre" element={<CanCrear />}/>
+        <Route path="/Canlis" element={<CanListar />}/>
+        <Route path="/Canadmin" element={<CanAdmin />}/>
 
         <Route path="/canact/: codigo" element={<CanActualizar />}/>
         
+        <Route path="/acer" element={<Acerca/>}/>
+
         <Route path="*" element={<NoEncontrado />}/>
        </Routes>
     )
